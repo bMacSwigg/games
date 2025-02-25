651d22619e7469b2f7249cc2918aad915b24289e
Brian McSwiggen <brian.mcswiggen@gmail.com>
2025-02-25 17:47:18 -0500

    Fleshing out Board

--- pysrc/engines/baghchal/board.py
+++ pysrc/engines/baghchal/board.py
@@ -1,21 +1,50 @@
 
+class IllegalMove(RuntimeError): pass
 
 class Position:
     def __init__(self, x, y):
-        if (x < 0 or x > 4):
+        if x < 0 or x > 4:
             raise RuntimeError("illegal x position")
-        if (y < 0 or y > 4):
+        if y < 0 or y > 4:
             raise RuntimeError("illegal y position")
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return "({},{})".format(self.x, self.y)
+
 class Board:
 
-    board = [[' '] * 5 for i in range(5)]
-    board[0][0] = 'T'
-    board[4][0] = 'T'
-    board[0][4] = 'T'
-    board[4][4] = 'T'
+    def __init__(self):
+        self.board = [[' '] * 5 for i in range(5)]
+        self.board[0][0] = 'T'
+        self.board[4][0] = 'T'
+        self.board[0][4] = 'T'
+        self.board[4][4] = 'T'
+
+    def get(self, pos: Position) -> None | str:
+        val = self.board[pos.x][pos.y]
+        if val == ' ':
+            return None
+        else:
+            return val
+
+    def set(self, pos: Position, val: str):
+        if val != 'T' and val != 'G':
+            raise IllegalMove("{} is not a valid piece".format(val))
+
+        if self.board[pos.x][pos.y] == ' ':
+            self.board[pos.x][pos.y] = val
+        else:
+            raise IllegalMove("Cannot set {} to {}, already set to {}".format(
+                                  pos, val, self.board[pos.x][pos.y]
+                              ))
+
+    def clear(self, pos: Position):
+        if self.board[pos.x][pos.y] != ' ':
+            self.board[pos.x][pos.y] = ' '
+        else:
+            raise IllegalMove("Cannot clear {}, already empty".format(pos))
 
     def visualize(self):
         print(self._visualize_row(0))
--- pysrc/engines/baghchal/board_test.py
+++ pysrc/engines/baghchal/board_test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from pysrc.engines.baghchal.board import Board, Position
+from pysrc.engines.baghchal.board import Board, Position, IllegalMove
+
 
 class TestPosition(unittest.TestCase):
 
@@ -33,6 +34,36 @@ class TestBoard(unittest.TestCase):
         self.assertFalse(Board._connected(Position(0, 0), Position(2, 0)))
         self.assertFalse(Board._connected(Position(0, 0), Position(0, 0)))
         self.assertFalse(Board._connected(Position(0, 1), Position(1, 0)))
+
+    def test_setValid(self):
+        b = Board()
+        b.set(Position(0, 1), 'G')
+        b.set(Position(2, 3), 'T')
+
+    def test_setInvalid(self):
+        b = Board()
+        with self.assertRaises(IllegalMove):
+            b.set(Position(0, 0), 'G')
+        with self.assertRaises(IllegalMove):
+            b.set(Position(2, 2), 'X')
+
+    def test_clearValid(self):
+        b = Board()
+        b.clear(Position(0, 0))
+
+    def test_clearInvalid(self):
+        b = Board()
+        with self.assertRaises(IllegalMove):
+            b.clear(Position(3, 4))
+
+    def test_get(self):
+        b = Board()
+        b.set(Position(0, 1), 'G')
+        b.clear(Position(0, 4))
+        self.assertEqual(b.get(Position(0, 0)), 'T')
+        self.assertEqual(b.get(Position(0, 1)), 'G')
+        self.assertIsNone(b.get(Position(0, 4)))
+        self.assertIsNone(b.get(Position(2, 2)))
 
 
 if __name__ == '__main__':
