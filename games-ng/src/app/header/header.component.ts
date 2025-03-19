407ec114183ab64216fc05878aa1b455b2ab727c
Brian McSwiggen <brian.mcswiggen@gmail.com>
2025-03-19 16:49:06 -0400

    Flesh out toolbar header

--- games-ng/src/app/header/header.component.ts
+++ games-ng/src/app/header/header.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  @Input() sidenav!: MatSidenav
 }
