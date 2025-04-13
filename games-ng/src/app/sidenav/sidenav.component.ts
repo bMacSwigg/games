2123eb5aa0b876546136a26a580a883a5c1b8e42
Brian McSwiggen <brian.mcswiggen@gmail.com>
2025-04-13 12:13:39 -0400

    Make sidenav route

--- games-ng/src/app/sidenav/sidenav.component.ts
+++ games-ng/src/app/sidenav/sidenav.component.ts
@@ -2,12 +2,13 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatListModule],
+  imports: [CommonModule, MatButtonModule, MatListModule, RouterModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
