eebbad62387757bf8398f8dd95d2be827b97e881
Brian McSwiggen <brian.mcswiggen@gmail.com>
2025-04-13 11:07:07 -0400

    Add game component

--- games-ng/src/app/app.routes.ts
+++ games-ng/src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import { BaghchalListComponent } from './baghchal-list/baghchal-list.component';
+import { BaghchalGameComponent } from './baghchal-game/baghchal-game.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: BaghchalListComponent,
     title: 'Bagh-Chal',
+  },
+  {
+    path: 'baghchal',
+    component: BaghchalListComponent,
+    title: 'Bagh-Chal',
+  },
+  {
+    path: 'baghchal/:gameId',
+    component: BaghchalGameComponent,
+    title: 'Bagh-Chal Game',
   },
 ];
---
+++ games-ng/src/app/baghchal-game/baghchal-game.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baghchal-game',
+  standalone: true,
+  imports: [],
+  templateUrl: './baghchal-game.component.html',
+  styleUrl: './baghchal-game.component.css'
+})
+export class BaghchalGameComponent {
+
+}
