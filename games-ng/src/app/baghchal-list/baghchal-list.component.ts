8f7335115a59af443f2083e7d8cbda539bf9c28f
Brian McSwiggen <brian.mcswiggen@gmail.com>
2025-04-13 12:11:38 -0400

    Show list of games

--- games-ng/src/app/baghchal-list/baghchal-list.component.ts
+++ games-ng/src/app/baghchal-list/baghchal-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GamesService } from '../games.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-baghchal-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatButtonModule, RouterModule],
   templateUrl: './baghchal-list.component.html',
   styleUrl: './baghchal-list.component.css'
 })
 export class BaghchalListComponent {
+  columnsToDisplay: string[] = ['tiger', 'goat', 'action'];
+  gamesList: Object[] = [];
+  gamesService: GamesService = inject(GamesService);
 
+  constructor() {
+    this.load();
+  }
+
+  load() {
+    this.gamesService.listGames().then(games => {
+      this.gamesList = games;
+    });
+  }
 }
--- games-ng/src/app/games.service.ts
+++ games-ng/src/app/games.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
+  baseUrl = 'https://games-869102415447.us-central1.run.app';
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
+
+  async listGames(): Promise<Object[]> {
+    const token = await this.auth.token();
+    if (!token) {
+      console.log('no token');
+      return [];
+    }
+
+    try {
+      let url = `${this.baseUrl}/v0/games/baghchal`;
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log(`Non-ok response when fetching games: ${response.status}`);
+      }
+    } catch (err) {
+      console.log(`Error when fetching games: ${err}`);
+    }
+    return [];
+  }
 }
