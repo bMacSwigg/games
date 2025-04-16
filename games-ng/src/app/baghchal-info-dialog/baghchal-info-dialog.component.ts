95371cae2d67af9eba65cd8834e0145ae35500b3
Brian McSwiggen <brian.mcswiggen@gmail.com>
2025-04-15 23:21:35 -0400

    Expand info with rules

--- games-ng/src/app/baghchal-info-dialog/baghchal-info-dialog.component.ts
+++ games-ng/src/app/baghchal-info-dialog/baghchal-info-dialog.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-baghchal-info-dialog',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatListModule, MatButtonModule],
   templateUrl: './baghchal-info-dialog.component.html',
   styleUrl: './baghchal-info-dialog.component.css'
 })
